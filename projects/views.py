91dd8dd0a0a7761cf28db6095dadd74fdbc83780
Steven <70606274+stevennm@users.noreply.github.com>
2020-09-13 06:51:26 -0300

    first commit

---
+++ projects/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Project
+
+
+# Create your views here.
+
+def index(request):
+    projects = Project.objects
+    return render(request, 'projects/index.html', {'projects': projects})
+
+
+def detail(request, project_id):
+    project_detail = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/detail.html', {'project': project_detail})
